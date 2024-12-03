158c8f9068aacf23ac9132495a505fe42d93609c
Brandon F <168985205+Alafa117@users.noreply.github.com>
2024-12-03 00:42:34 -0500

    2

--- Front/src/components/crearusers.jsx
+++ Front/src/components/crearusers.jsx
@@ -21,7 +21,7 @@ function Crearusuarios() {
             return;
         }
 
-        fetch(`https://yutu-1-front.vercel.app/papa/crear`, {
+        fetch(`https://blackback01.vercel.app/papa/crear`, {
             method: 'POST',
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({ 
